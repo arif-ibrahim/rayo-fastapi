756fb255f8effe0c061a2f167424c0e9f09cad51
Arif Ibrahim <arif.ibrahm@gmail.com>
2023-06-11 00:28:36 +0600

    show product api done

--- rayoo/repository/product.py
+++ rayoo/repository/product.py
@@ -1,11 +1,19 @@
+from fastapi import HTTPException, status
 from sqlalchemy.orm import Session
 from .. import schemas, models
 
 
-
-
 def create(request: schemas.Product, db: Session):
-    new_product = models.Product(title=request.title, price=request.price, pictures=request.pictures, status=request.status, quantity=request.quantity, color=request.color, size=request.size, description=request.description)
+    new_product = models.Product(
+        title=request.title,
+        price=request.price,
+        pictures=request.pictures,
+        status=request.status,
+        quantity=request.quantity,
+        color=request.color,
+        size=request.size,
+        description=request.description,
+    )
     db.add(new_product)
     db.commit()
     db.refresh(new_product)
@@ -14,5 +22,14 @@ def create(request: schemas.Product, db: Session):
 
 def get_all(db: Session):
     products = db.query(models.Product).all()
-    print(products)
     return products
+
+
+def show(id: int, db: Session):
+    product = db.query(models.Product).filter(models.Product.id == id).first()
+    if not product:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail=f"Product with the id {id} is not available",
+        )
+    return product
--- rayoo/routers/product.py
+++ rayoo/routers/product.py
@@ -5,14 +5,21 @@ from .. import database, schemas
 from ..repository import product
 
 
-router = APIRouter(prefix="/product", tags=['Products'])
+router = APIRouter(prefix="/product", tags=["Products"])
 
 get_db = database.get_db
 
-@router.post('/', status_code=status.HTTP_201_CREATED)
+
+@router.get("/", response_model=List[schemas.ShowProduct])
+def all(db: Session = Depends(get_db)):
+    return product.get_all(db)
+
+
+@router.post("/", status_code=status.HTTP_201_CREATED)
 def create(request: schemas.Product, db: Session = Depends(get_db)):
     return product.create(request, db)
 
-@router.get('/',response_model=List[schemas.ShowProduct])
-def all(db: Session = Depends(get_db)):
-    return product.get_all(db)
+
+@router.get("/{id}", response_model=schemas.ShowProduct)
+def show(id: int, db: Session = Depends(get_db)):
+    return product.show(id, db)
