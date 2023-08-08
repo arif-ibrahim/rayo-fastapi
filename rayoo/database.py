038a6ee942d51dc6a564f993b29c061117cfd668
Arif Ibrahim <arif.ibrahm@gmail.com>
2023-08-09 00:45:14 +0600

    database credentials move to .env file

--- rayoo/database.py
+++ rayoo/database.py
@@ -1,15 +1,25 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:123456@localhost/postgres"
+
+load_dotenv()
+
+database_url = os.getenv("SQLALCHEMY_DATABASE_URL")
 
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+engine = create_engine(database_url)
 
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False,)
+SessionLocal = sessionmaker(
+    bind=engine,
+    autocommit=False,
+    autoflush=False,
+)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
